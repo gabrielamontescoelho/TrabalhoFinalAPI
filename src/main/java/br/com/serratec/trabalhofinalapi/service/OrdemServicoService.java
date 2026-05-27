8519e6ce8f9544b296f71201940ae315f536e6c0
lc-lira <lbrancolira@gmail.com>
2026-05-27 16:54:41 -0300

    salvando parte dos metodos do ordemservicoService

--- src/main/java/br/com/serratec/trabalhofinalapi/service/OrdemServicoService.java
+++ src/main/java/br/com/serratec/trabalhofinalapi/service/OrdemServicoService.java
@@ -11,6 +11,7 @@ import br.com.serratec.trabalhofinalapi.dto.ItemOrdemServicoResponseDTO;
 import br.com.serratec.trabalhofinalapi.dto.OrdemServicoRequestDTO;
 import br.com.serratec.trabalhofinalapi.dto.OrdemServicoResponseDTO;
 import br.com.serratec.trabalhofinalapi.dto.VeiculoResponseDTO;
+import br.com.serratec.trabalhofinalapi.model.Cliente;
 import br.com.serratec.trabalhofinalapi.model.OrdemServico;
 import br.com.serratec.trabalhofinalapi.repository.ClienteRepository;
 import br.com.serratec.trabalhofinalapi.repository.OrdemServicoRepository;
@@ -36,7 +37,16 @@ public class OrdemServicoService {
     }
 
     public OrdemServicoResponseDTO buscar(Long id) {
-        return null;
+        OrdemServico os = ordemRepository.findById(id).orElseThrow(() -> new RuntimeException("Ordem de Serviço não encontrada!"));  
+        
+        List<ItemOrdemServicoResponseDTO> itemDto = os.getItens().stream().map(item -> new ItemOrdemServicoResponseDTO(
+            item.getId(),
+            item.getServico().getId(),
+            item.getQuantidade(),
+            item.getValorServico(),
+            item.getDesconto(),
+            item.getSubtotal()
+        )).toList();
+        ClienteResponseDto clienteDto = new ClienteResponseDto(os.getCliente());
     }
-
 }
